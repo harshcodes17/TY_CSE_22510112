0c4d655e40615599e10303aed826d6fcab791eae
harshcodes17 <harshabamane1718@gmail.com>
2024-08-05 11:30:35 +0530

    added Assign 1 and Assign 2

--- DAA_LAB/Assign 1/q1.cpp
+++ DAA_LAB/Assign 1/q1.cpp
@@ -21,18 +21,18 @@ void merge(vector<int>& v1, int m, vector<int>& v2, int n) {
     }
 }
 
-int mv1in() {
-    vector<int> v1 = {1, 2, 3, 4, 5, 0, 0, 0, 0, 0}; 
-    vector<int> v2 = {2, 4, 6, 8, 10}; 
-
-    int m = 5; 
-    int n = 5;
+int main() {
+    vector<int> v1 = {1, 3, 5, 0, 0, 0}; // v1 has extra space for v2 elements
+    vector<int> v2 = {2, 4, 6};
+    int m = 3; // Number of valid elements in v1
+    int n = 3; // Number of elements in v2
 
     merge(v1, m, v2, n);
 
-    for (int num : v1){
+    for (int num : v1) {
         cout << num << " ";
     }
+    cout << endl;
 
     return 0;
 }
---
+++ DAA_LAB/Assign 2/q1.cpp
@@ -0,0 +1,40 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+    int n;
+    cout << "Enter the number of days: ";
+    cin>>n;
+    vector<int>v(n);
+    cout << "Enter the working hour per day: ";
+    for(int i=0;i<n;i++){
+        cin>>v[i];
+    }
+    int ans=0;
+    vector<int>bin;
+    for(int i=0;i<n;i++){
+        if(v[i]>6){
+            bin.push_back(1);
+        }
+        else{
+            bin.push_back(0);
+        }
+    }
+    int sum = 0;
+
+    for(int i=0;i<n;i++){
+        if(bin[i]==1){
+            sum++;
+        }
+        else{
+            ans = max(ans,sum);
+            sum=0;
+        }
+    }
+    ans = max(ans,sum);
+    cout<<"Rating of the employee is: ";
+    cout<<ans<<endl;
+    return 0;
+
+}
